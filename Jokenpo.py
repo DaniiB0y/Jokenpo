7dc1fd376c0a81ffab88ff37153366b7b6ea7ba8
DannyBoy <neo3nt3r@gmail.com>
2020-11-06 16:05:13 -0300

    Pouca linha mas com mesmas funções
    Coloquei pouca linha de código que facilita na edição, também modifiquei o método de verificação de pedra papel e tesoura para que fique de forma mais simples e intuitiva

--- Jokenpo.py
+++ Jokenpo.py
@@ -1,106 +1,41 @@
-#!/usr/bin/env python3
+from random import randint
 import os
-import random
-
-escolha = int(input('\n                    '+'\033[1;31m'+'  ✌ '+'\033[1;36m'+'[-   jokenpô  じゃんけんぽん      -]'+'✌  '+'\n'+'\033[1;97m'+'\n1-Jogar'+'\n2-Sair'+'\033[1;36m'+'\n\nEscolha: '))
-
-def main(): # Começo
-    maquina = random.choice(["Pedra", "Papel", "Tesoura"]) # Maquina escolhe aleatoriamente entre os 3
-
-
-    if escolha == 1:  # Se a escolha for igual a 1 Inicia o game
-            os.system('clear')
-            x = str(input('************************************************\nOBS:Colocar da forma que está escrito abaixo.\n************************************************\n\n\033[1;36m'+'----------------------------------------'+"\nPedra, Papel ou Tesoura?\n"+'----------------------------------------'+'\033[1;31m'+'\n\nEscolha: '))
-
-
-    elif escolha == 2: # Se a escolha for igual a 2, limpa a tela e fecha.
-        os.system('clear')
-        exit(0)
-#
-# Papel
-#
-    if x == 'Papel' or x == 'papel' or x == 'PAPEL' and maquina == 'Pedra':
-            os.system('clear')
-            print('\033[1;97m'+'----------------------------------------')
-            print('\033[1;36m'+'Papel embrulha Pedra, Você ganhou!')
-            print('\033[1;97m'+'----------------------------------------')
-            os.system('sleep 3')
-            main()
-
-
-    elif x == 'Papel' or x == 'papel' or x == 'PAPEL' and maquina == 'Tesoura':
-        os.system('clear')
+c  = 0
+escolha = 1
+perdeu = 0
+ganhou = 0
+empate = 0
+while escolha != 4:
+    dic = ["","Pedra", "Papel", "Tesoura"]
+    bot = randint(1, 3)
+    if c == 0:
+        escolha = int(input('\n                    '+'\033[1;31m'+'  ✌ '+'\033[1;36m'+'[-   jokenpô  じゃんけんぽん      -]'+'✌  '+'\n'+'\033[1;97m'+'\n1-Jogar'+'\n4-Sair'+'\033[1;36m'+'\n\nEscolha: '))
+        os.system('cls')
+        c+=1
+    x = int(input("Pedra: 1\nPapel: 2\nTesoura: 3\n~>"))
+    os.system('cls')
+    if dic[x] == dic[bot]:
         print('\033[1;97m'+'----------------------------------------')
-        print('\033[1;31m'+'Tesoura corta Papel, Você perdeu.')
+        print('\033[1;97m'+f'{dic[x]} = {dic[bot]}, EMPATE!')
+        empate += 1
+    elif dic[bot] != "Papel" and x == 1:
         print('\033[1;97m'+'----------------------------------------')
-        os.system('sleep 3')
-        main()
-
-
-    elif x == 'Papel' or x == 'papel' or x == 'PAPEL' and maquina == 'Papel':
-        os.system('clear')
+        print(f'\033[1;97m'+f'Pedra ganha de {dic[bot]}, você ganhou!')
         print('\033[1;97m'+'----------------------------------------')
-        print('\033[1;33m'+'Os dois colocaram Papel, Deu empate.')
+        ganhou += 1
+    elif dic[bot] != "Pedra" and x == 3:
         print('\033[1;97m'+'----------------------------------------')
-        os.system('sleep 3')
-        main()
-
-
-#
-# Tesoura
-#
-    if x == 'Tesoura' or x == 'tesoura' or x == 'TESOURA' and maquina == 'Papel':
-            os.system('clear')
-            print('\033[1;97m'+'----------------------------------------')
-            print('\033[1;36m'+'Tesoura corta Papel, Você ganhou!')
-            print('\033[1;97m'+'----------------------------------------')
-            os.system('sleep 3')
-            main()
-
-
-    elif x == 'Tesoura' or x == 'tesoura' or x == 'TESOURA' and maquina == 'Pedra':
-        os.system('clear')
+        print(f'\033[1;97m'+f'Tesoura ganha de {dic[bot]}, você ganhou!')
         print('\033[1;97m'+'----------------------------------------')
-        print('\033[1;31m'+'Pedra quebra Tesoura, Você perdeu.')
+    elif dic[bot] != "Tesoura" and x == 2:
         print('\033[1;97m'+'----------------------------------------')
-        os.system('sleep 3')
-        main()
-
-
-    elif x == 'Tesoura' or x == 'tesoura' or x == 'TESOURA' and maquina == 'Tesoura':
-        os.system('clear')
+        print(f'\033[1;97m'+f'Papel ganha de {dic[bot]}, você ganhou!')
         print('\033[1;97m'+'----------------------------------------')
-        print('\033[1;33m'+'Os dois colocaram Tesoura, Deu empate.')
+        ganhou += 1
+    else:
         print('\033[1;97m'+'----------------------------------------')
-        os.system('sleep 3')
-        main()
-#
-# Pedra
-#
-
-    if x == 'Pedra' or x == 'pedra' or x == 'PEDRA' and maquina == 'Tesoura':
-            os.system('clear')
-            print('\033[1;97m'+'----------------------------------------')
-            print('\033[1;36m'+'Pedra quebra Tesoura, Você ganhou!')
-            print('\033[1;97m'+'----------------------------------------')
-            os.system('sleep 3')
-            main()
-
-
-    elif x == 'Pedra' or x == 'pedra' or x == 'PEDRA' and maquina == 'Papel':
-        os.system('clear')
+        print(f"Perdeu! {dic[x]} perde para {dic[bot]}")
         print('\033[1;97m'+'----------------------------------------')
-        print('\033[1;31m'+'Papel embrulha Pedra, Você perdeu.')
-        print('\033[1;97m'+'----------------------------------------')
-        os.system('sleep 3')
-        main()
+        perdeu += 1
+    print(f"Perdeu {perdeu} Ganhou {ganhou} Empate: {empate} vezes jogadas {c}")
 
-
-    elif x == 'Pedra' or x == 'pedra' or x == 'PEDRA' and maquina == 'Pedra':
-        os.system('clear')
-        print('\033[1;97m'+'----------------------------------------')
-        print('\033[1;33m'+'Os dois colocaram Pedra, Deu empate.')
-        print('\033[1;97m'+'----------------------------------------')
-        os.system('sleep 3')
-        main()
-main()
